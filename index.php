39596117d9eca6e59556964ab8ae84611a7444e6
sachato <sachabasse@gmail.com>
2021-09-16 09:35:37 +0200

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <form action="./compte/inscription.php">
+        <input type="submit" value="Inscription">
+    </form>
+    <form action="./compte/connexion.php">
+        <input type="submit" value="Connexion">
+    </form>
+</body>
+</html>
